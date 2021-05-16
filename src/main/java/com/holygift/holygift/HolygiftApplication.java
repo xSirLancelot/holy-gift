8baeec2dc7e764dda19074796fe5df03f8b8f3b7
Xavier CJ <72420296+xSirLancelot@users.noreply.github.com>
2021-05-16 12:12:23 -0600

    Initial project structure
    Xavier Carrizo- Initial project structure
    Holy Gift
    
    - Sending initial structure for Spring Boot project

---
+++ src/main/java/com/holygift/holygift/HolygiftApplication.java
@@ -0,0 +1,13 @@
+package com.holygift.holygift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolygiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolygiftApplication.class, args);
+	}
+
+}
